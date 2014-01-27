031a1a151370f74a73ee5bc71871ee3f2fe7aa24
praetoriansentry <praetoriansentry@gmail.com>
2014-01-26 21:37:03 -0500

    Adding a worker thread
    Probably did this completely wrong, but I wanted to add a worker
    thread so that the GUI wouldn't lock up for long SQL queries.

--- TSQLFormatter/TSQLFormatter/Form1.cs
+++ TSQLFormatter/TSQLFormatter/Form1.cs
@@ -19,8 +19,38 @@ namespace TSQLFormatter
 
         private void formatSqlButton_Click(object sender, EventArgs e)
         {
-            Formatter f = new Formatter();
-            outputSql.Text = f.Format(inputSql.Text);
+
+            // Take from here: http://stackoverflow.com/questions/363377/how-do-i-run-a-simple-bit-of-code-in-a-new-thread
+
+            BackgroundWorker bw = new BackgroundWorker();
+
+            // this allows our worker to report progress during work
+            bw.WorkerReportsProgress = true;
+
+            // what to do in the background thread
+            bw.DoWork += new DoWorkEventHandler(
+            delegate(object o, DoWorkEventArgs args)
+            {
+                BackgroundWorker b = o as BackgroundWorker;
+
+                Formatter f = new Formatter();
+                string sqlString = "";
+                if (inputSql.InvokeRequired)
+                {
+                    inputSql.Invoke(new MethodInvoker(delegate { sqlString = inputSql.Text; }));
+                }
+
+                args.Result = f.Format(sqlString);
+
+            });
+
+            // what to do when worker completes its task (notify the user)
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(
+            delegate(object o, RunWorkerCompletedEventArgs args)
+            {
+                outputSql.Text = args.Result.ToString();
+            });
+            bw.RunWorkerAsync();
         }
 
     }
